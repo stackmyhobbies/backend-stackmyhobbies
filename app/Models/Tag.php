0b91a7737e442adfc4dd2fc959a630bde38ca8ac
Jorge Andres Salgado Echeverria <jasen.estudio@gmail.com>
2025-06-06 00:56:04 -0500

    Created pivot table to manage the many-to-many relationship between content items and tags.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     //
+
+    public function contentItems()
+    {
+        return $this->belongsToMany(ContentItem::class, 'content_tag', 'tag_id', 'content_item_id')
+            ->withTimestamps()
+            ->withPivot('status');
+    }
 }
--- app/Models/contentItem.php
+++ app/Models/contentItem.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class contentItem extends Model
 {
     //
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'content_tag', 'content_item_id', 'tag_id')
+            ->withTimestamps()
+            ->withPivot('status');
+    }
 }
