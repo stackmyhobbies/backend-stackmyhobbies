c050b9e5c81ab29c69320d777fd5f4793314b276
Jorge Andres Salgado Echeverria <jasen.estudio@gmail.com>
2025-07-11 13:00:55 -0500

    fix userController and userService with tryCatch

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\ApiResponseClass;
 use App\Http\Requests\User\StoreUserRequest;
 use App\Http\Requests\User\UpdateUserRequest;
 use App\Services\UserService;
+use App\Support\TryHttpCatch;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -19,29 +22,48 @@ class UserController extends Controller
     //
     public function index()
     {
-        return $this->_userService->index();
+        return TryHttpCatch::handle(function () {
+            $users =  $this->_userService->index();
+            return ApiResponseClass::sendResponse($users, "Users loaded successfully", Response::HTTP_OK);
+        });
     }
 
     public function show($id)
     {
-        return $this->_userService->show($id);
+        $user = $this->_userService->show($id);
+        return ApiResponseClass::sendResponse($user, "User loaded successfully.", Response::HTTP_OK);
     }
 
     public function store(StoreUserRequest $request)
     {
         $validated = $request->validated();
 
-        return $this->_userService->store($validated);
+        return TryHttpCatch::handle(
+            function () use ($validated) {
+                $user = $this->_userService->store($validated);
+                return ApiResponseClass::sendResponse($user, 'User created successfully.', Response::HTTP_CREATED);
+            }
+        );
     }
 
     public function update(UpdateUserRequest $request, $id)
     {
         $validated = $request->validated();
-        return $this->_userService->update($validated, $id);
+        return TryHttpCatch::handle(
+            function () use ($validated, $id) {
+                $user = $this->_userService->update($validated, $id);
+                return ApiResponseClass::sendResponse($user, 'User updated successfully.', Response::HTTP_OK);
+            }
+        );
     }
 
     public function destroy($id)
     {
-        return $this->_userService->destroy($id);
+        return TryHttpCatch::handle(
+            function () use ($id) {
+                $this->_userService->destroy($id);
+                return ApiResponseClass::sendResponse(null, 'User deleted successfully', Response::HTTP_OK);
+            }
+        );
     }
 }
